8bca4575ecb9f0cba3b2bb479ddf4c7b5551e54b
gaikarsarika <gaikarsarika233.com>
2023-02-03 22:50:13 +0530

    change by sariks

---
+++ src/main/java/com/example/demo/Repository4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Repository4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Repository4Application.class, args);
+	}
+
+}
